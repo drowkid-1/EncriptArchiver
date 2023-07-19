d1e687cca2a55854cf122f97b5ef572816d1b1a6
DrowKid <135056994+drowkid-1@users.noreply.github.com>
2023-07-18 23:04:45 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center max-w-3xl px-8 mx-auto mt-8 sm:min-h-screen sm:mt-0 sm:px-0">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center">
           <Link
-            href="https://github.com/chronark/envshare"
+            href="https://github.com/drowkid-1/"
             className="text-zinc-400 relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-zinc-100/10 hover:ring-zinc-100/30 duration-150"
           >
             EnvShare is Open Source on{" "}
@@ -19,24 +19,23 @@ export default function Home() {
         </div>
         <div>
           <h1 className="py-4 text-5xl font-bold tracking-tight text-center text-transparent bg-gradient-to-t bg-clip-text from-zinc-100/50 to-white sm:text-7xl">
-            Share Environment Variables Securely
+            Encripta tus archivos con protocolos AES-GCM 
           </h1>
           <p className="mt-6 leading-5 text-zinc-600 sm:text-center">
-            Your document is encrypted in your browser before being stored for a limited period of time and read
-            operations. Unencrypted data never leaves your browser.
+            Al encriptar un archivo, se generará un enlace el cual contiene un ID, único ID que podrá acceder al documento con más de 15 dígitos.
           </p>
           <div className="flex flex-col justify-center gap-4 mx-auto mt-8 sm:flex-row sm:max-w-lg ">
             <Link
-              href="/deploy"
+              href="/share"
               className="sm:w-1/2 sm:text-center inline-block space-x-2 rounded px-4 py-1.5 md:py-2 text-base font-semibold leading-7 text-white  ring-1 ring-zinc-600 hover:bg-white hover:text-zinc-900 duration-150 hover:ring-white hover:drop-shadow-cta"
             >
-              Deploy
+              Probar EncriptArchiver 
             </Link>
             <Link
-              href="/share"
+              href="https://github.com/drowkid-1/EncriptArchiver"
               className="sm:w-1/2 sm:text-center inline-block transition-all space-x-2  rounded px-4 py-1.5 md:py-2 text-base font-semibold leading-7 text-zinc-800   bg-zinc-50 ring-1 ring-transparent hover:text-zinc-100 hover:ring-zinc-600/80  hover:bg-zinc-900/20 duration-150 hover:drop-shadow-cta"
             >
-              <span>Share</span>
+              <span>Clonar repositorio</span>
               <span aria-hidden="true">&rarr;</span>
             </Link>
           </div>
